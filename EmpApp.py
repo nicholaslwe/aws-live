a807b66e9c1bce569f8702a1a84bd9c2aa1b0e55
nicholaslwe <130641110+nicholaslwe@users.noreply.github.com>
2023-05-08 18:58:24 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -23,7 +23,7 @@ table = 'Leave'
 # Index
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('ViewLeave.html')
+    return render_template('editLeave.html')
 
 # About Us
 @app.route("/about", methods=['POST'])
